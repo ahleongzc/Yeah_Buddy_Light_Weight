ebd4408792d53284d5f94f6be2354cf865bbbd95
Leong Zhe Cheng <90683902+ahleongzc@users.noreply.github.com>
2022-05-14 17:40:56 +0800

    Initial Commit

---
+++ WorkoutTracker/WorkoutTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutTrackerApp.swift
+//  WorkoutTracker
+//
+//  Created by Leong Zhe Cheng on 14/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
